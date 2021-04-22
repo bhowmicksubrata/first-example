6ff42631e7304e43368f534c0db0649eb3934885
bhowmicksubrata <subrata.bhowmick.mca@gmail.com>
2021-04-23 01:08:34 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 // Import stylesheets
 import "./style.css";
+import "./script/first-script.js";
 
-// Write Javascript code!
-$("h1").click(function() {
-  console.log("Function called");
-  alert("The paragraph was clicked.");
-  let exp = document.getElementById("my-heading");
-  exp.innerHTML = "hello";
-
-  console.log("Function completed");
-});
-
-$("button").click(function() {
-  alert("The paragraph was clicked.");
-  console.log("The paragraph was clicked.");
-});
+// when adding a new java script file
+// always import it here to work in side stackblitz
